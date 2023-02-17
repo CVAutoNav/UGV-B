dbf5811cb717b2ab207ac9012cf04475fb92a090
Sumukha21 <sumukha1996@gmail.com>
2023-02-17 12:41:11 -0800

    Version 2 of jackal control using python

--- src/ugv_b/scripts/jackal_control.py
+++ src/ugv_b/scripts/jackal_control.py
@@ -1,27 +1,30 @@
 #! /usr/bin/env python
 
 import rospy
+import cv2
+import keyboard
 from geometry_msgs.msg import Twist
 
 if __name__ =="__main1__":
   rospy.init_node('topic_pu7blisher')
   pub = rospy.Publisher('/jackal_velocity_controller/cmd_vel', Twist, queue_size=1)
-  rate = rospy.Rate(2)
+  rate = rospy.Rate(15)
   move = Twist() # defining the way we can allocate the values
-  move.linear.x = 0.5 # allocating the values in x direction - linear
-  move.angular.z = 0.5  # allocating the values in z direction - angular
+  move.linear.x = 0.05 # allocating the values in x direction - linear
+  move.angular.z = 0.05  # allocating the values in z direction - angular
 
   while not rospy.is_shutdown():
     pub.publish(move)
     rate.sleep()
 
+
 if __name__ =="__main__":
   rospy.init_node('topic_pu7blisher')
   pub = rospy.Publisher('/jackal_velocity_controller/cmd_vel', Twist, queue_size=1)
-  rate = rospy.Rate(2)
+  rate = rospy.Rate(10)
   move = Twist()  # defining the way we can allocate the values
   while not rospy.is_shutdown():
-    movement_input = raw_input("Enter the movement coordinates in the following format: x y z roll pitch yaw : \n").split()
+    movement_input = raw_input("Enter the movement coordinates in the following format: x y z roll pitch yaw repeat_rate(optional): \n").split()
     movement_input = [float(i) for i in movement_input]
     print(movement_input)
     move.linear.x = movement_input[0]
@@ -30,5 +33,13 @@ if __name__ =="__main__":
     move.angular.x = movement_input[3]
     move.angular.y = movement_input[4]
     move.angular.z = movement_input[5]
-    pub.publish(move)
-    rate.sleep()
+    if len(movement_input) == 7:
+        while True:
+	    pub.publish(move)
+	    #rate.sleep()
+	    if keyboard.is_pressed('q'):
+		print("q was pressed, stopping")
+                break
+    else:
+        pub.publish(move)
+        rate.sleep()
